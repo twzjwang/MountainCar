8ec0d86ed4645d93cee52817795e9c31b19d5717
twzjwang <twzjwang@gmail.com>
2018-06-11 15:08:12 +0800

    Add Q learning

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import numpy as np
+import random
 import pandas as pd
 import gym
 
+def toPos(x, y):
+    return int((y + 0.1) / 0.2 * 100) * 100 + int((x + 1.5) / 2.5 * 100)
+
 env = gym.make('MountainCar-v0')
-for i_episode in range(20):
+Q = np.zeros([100 * 100, env.action_space.n])
+
+record = np.zeros(100)
+for i in range(30000):
     observation = env.reset()
-    for t in range(100):
-        env.render()
-        print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
+    pos = toPos(observation[0], observation[1])
+    reward_sum = 0.0
+    for t in range(300):
+        #env.render()
+        action = np.argmax(Q[pos,:]+random.randint(0,2)*(1.0/(i+1)))
+        observation, new_reward, done, info = env.step(action)
+        new_pos = toPos(observation[0], observation[1])
+        Q[pos, action] = new_reward + Q[pos, action] + np.max(Q[new_pos, :]) - Q[pos, action]
+        reward_sum += new_reward
+        pos = new_pos
+        if i % 5000 == 0:
+            env.render()
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
             break
+    if i % 100 == 0:
+        mean = record.mean()
+        print(i, mean)
+    else:
+        record[i % 100] = reward_sum
